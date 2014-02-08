56a8d40161ce37153ece8740fdd4edb173e78a40
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-02-08 13:22:04 -0600

    Support reloading of configuration

--- txircd/config.py
+++ txircd/config.py
@@ -7,44 +7,48 @@ _defaults = {
 
 class Config(object):
     def __init__(self, configFileName):
-        self._configData = {}
-        self._readConfig(configFileName)
+        self._fileName = configFileName
+        self.reload()
+    
+    def reload(self):
+        self._configData = self._readConfig(self._fileName)
         for key, val in _defaults.iteritems():
             if key not in self._configData:
                 self._configData[key] = val
     
     def _readConfig(self, fileName):
+        configData = {}
         try:
             with open(fileName, 'r') as configFile:
                 configData = yaml.safe_load(configFile)
         except Exception as e:
             raise ConfigReadError (fileName, e)
-        for key, val in configData.iteritems():
-            if key == "include":
-                continue
-            if key not in self._configData:
-                self._configData[key] = val
-            elif not isinstance(self._configData[key], basestring): # Let's try to merge them if they're lists
-                if isinstance(val, basestring):
-                    raise ConfigReadError(fileName, "The included configuration file tried to merge a non-string with a string.")
-                try: # Make sure both things we're merging are still iterable types
-                    iter(self._configData[key])
-                    iter(val)
-                except TypeError:
-                    pass # Simply don't merge them if they're not
-                else:
-                    try:
-                        self._configData[key] += val
-                    except TypeError: # They can't be merged with +=; we'll try them as dicts and then fail if they're not that
-                        try:
-                            for subkey, subval in val.iteritems():
-                                if subkey not in self._configData[key]:
-                                    self._configData[key][subkey] = subval
-                        except AttributeError, TypeError: # They weren't both dicts in this case, but were still both iterable; needs resolved by user.
-                            raise ConfigReadError(fileName, "The variable {} could not successfully be merged.".format(key))
         if "include" in configData:
             for fileName in configData["include"]:
-                self._readConfig(fileName)
+                includeConfig = self._readConfig(fileName)
+                for key, val in includeConfig.iteritems():
+                    if key not in configData:
+                        configData[key] = val
+                    elif not isinstance(configData[key], basestring): # Let's try to merge them if they're collections
+                        if isinstance(val, basestring):
+                            raise ConfigReadError(fileName, "The included configuration file tried to merge a non-string with a string.")
+                        try: # Make sure both things we're merging are still iterable types (not numbers or whatever)
+                            iter(configData[key])
+                            iter(val)
+                        except TypeError:
+                            pass # Just don't merge them if they're not
+                        else:
+                            try:
+                                configData[key] += val # Merge with the + operator
+                            except TypeError: # Except that some collections (dicts) can't
+                                try:
+                                    for subkey, subval in val.iteritems(): # So merge them manually
+                                        if subkey not in configData[key]:
+                                            configData[key][subkey] = subval
+                                except AttributeError, TypeError: # If either of these, they weren't both dicts (but were still iterable); requires user to resolve
+                                    raise ConfigReadError(fileName, "The variable {} could not be successfully merged across files.".format(key))
+            del configData["include"]
+        return configData
     
     def __len__(self):
         return len(self._configData)
