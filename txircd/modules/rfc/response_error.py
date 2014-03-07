d6120537ec982f50d08fa188e91c68c023809db3
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-03-06 21:33:33 -0600

    Send ERROR when the user disconnects

---
+++ txircd/modules/rfc/response_error.py
@@ -0,0 +1,17 @@
+from twisted.plugin import IPlugin
+from txircd.module_interface import IModuleData, ModuleData
+from zope.interface import implements
+
+class ErrorResponse(ModuleData):
+    implements(IPlugin, IModuleData)
+    
+    name = "errorResponse"
+    core = True
+    
+    def actions(self):
+        return [("quit", 10, self.sendError)]
+    
+    def sendError(self, user, reason):
+        user.sendMessage("ERROR", ":Closing Link: {}@{} [{}]".format(user.ident, user.host, reason), to=None, prefix=None)
+
+errorResponse = ErrorResponse()
