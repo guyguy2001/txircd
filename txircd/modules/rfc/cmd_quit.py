2960e7c0d54980fad32499aa700733f426deee63
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-03-09 15:21:01 -0500

    Make QUIT send notice and work across servers

--- txircd/modules/rfc/cmd_quit.py
+++ txircd/modules/rfc/cmd_quit.py
@@ -12,11 +12,35 @@ class QuitCommand(ModuleData, Command):
     def hookIRCd(self, ircd):
         self.ircd = ircd
     
+    def actions(self):
+        return [ ("quitmessage", 10, self.sendQuitMessage),
+                ("remotequitrequest", 10, self.sendRQuit),
+                ("remotequit", 10, self.propagateQuit) ]
+    
     def userCommands(self):
         return [ ("QUIT", 1, UserQuit(self.ircd)) ]
     
     def serverCommands(self):
-        return [ ("QUIT", 1, ServerQuit(self.ircd)) ]
+        return [ ("QUIT", 1, ServerQuit(self.ircd)),
+                ("RQUIT", 1, RemoteQuit(self.ircd)) ]
+    
+    def sendQuitMessage(self, user, reason, sendUserList):
+        hostmask = user.hostmask()
+        for destUser in sendUserList:
+            destUser.sendMessage("QUIT", ":{}".format(reason), to=None, prefix=hostmask)
+        del sendUserList[:]
+    
+    def sendRQuit(self, user, reason):
+        self.ircd.servers[user.uuid[:3]].sendMessage("RQUIT", ":{}".format(reason), prefix=user.uuid)
+        return True
+    
+    def propagateQuit(self, user, reason):
+        fromServer = self.ircd.servers[user.uuid[:3]]
+        while fromServer.nextClosest != self.ircd.serverID:
+            fromServer = self.ircd.servers[fromServer.nextClosest]
+        for server in self.ircd.servers.itervalues():
+            if server != fromServer and server.nextClosest == self.ircd.serverID:
+                server.sendMessage("QUIT", ":{}".format(reason), prefix=user.uuid)
 
 class UserQuit(Command):
     implements(ICommand)
@@ -61,5 +85,28 @@ class ServerQuit(Command):
     def execute(self, server, data):
         data["user"].disconnect(data["reason"], True)
         return True
+
+class RemoteQuit(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, server, params, prefix, tags):
+        if prefix not in self.ircd.users:
+            return None
+        if len(params) != 1:
+            return None
+        return {
+            "user": self.ircd.users[prefix],
+            "reason": params[0]
+        }
+    
+    def execute(self, server, data):
+        user = data["user"]
+        if user.uuid[:3] == self.ircd.serverID:
+            user.disconnect(data["reason"])
+            return True
+        self.ircd.servers[user.uuid[:3]].sendMessage("RQUIT", ":{}".format(data["reason"]), prefix=user.uuid)
 
 quitCommand = QuitCommand()
