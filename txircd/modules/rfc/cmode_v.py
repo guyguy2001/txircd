3fb4cd9ae9ce6d4a778d1f148f9644785092a8dd
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-04-11 23:48:17 -0500

    Implement channel voice mode

---
+++ txircd/modules/rfc/cmode_v.py
@@ -0,0 +1,15 @@
+from twisted.plugin import IPlugin
+from txircd.module_interface import IMode, IModuleData, Mode, ModuleData
+from txircd.utils import ModeType
+from zope.interface import implements
+
+class VoiceMode(ModuleData, Mode):
+    implements(IPlugin, IModuleData, IMode)
+    
+    name = "ChanVoiceMode"
+    core = True
+    
+    def channelModes(self):
+        return [ ("v", ModeType.Status, self, 10, "+") ]
+
+voiceMode = VoiceMode()
