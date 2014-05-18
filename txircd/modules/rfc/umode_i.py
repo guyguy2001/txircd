52982c735f729ddf0a9c020d495906c4a4899462
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-05-18 17:57:01 -0500

    Make the invisible check action not necessarily require an accompanying channel

--- txircd/modules/rfc/umode_i.py
+++ txircd/modules/rfc/umode_i.py
@@ -8,24 +8,43 @@ class InvisibleMode(ModuleData, Mode):
     
     name = "InvisibleMode"
     core = True
-    affectedActions = [ "showchanneluser" ]
+    affectedActions = [ "showchanneluser", "showuser" ]
     
     def actions(self):
-        return [ ("modeactioncheck-user-i-showchanneluser", 1, self.isInvisible) ]
+        return [ ("modeactioncheck-user-i-showchanneluser", 1, self.isInvisibleChan),
+                ("modeactioncheck-user-i-showuser", 1, self.isInvisibleUser) ]
     
     def userModes(self):
         return [ ("i", ModeType.NoParam, self) ]
     
-    def isInvisible(self, user, channel, fromUser, userSeeing):
+    def isInvisibleChan(self, user, channel, fromUser, userSeeing):
         if "i" in user.modes:
             return True
         return None
     
-    def apply(self, actionName, user, param, channel, fromUser, sameUser):
+    def isInvisibleUser(self, user, fromUser, userSeeing):
+        if "i" in user.modes:
+            return True
+        return None
+    
+    def apply(self, actionName, user, param, *params):
+        if actionName == "showchanneluser":
+            return self.applyChannels(user, *params)
+        return self.applyUsers(user, *params)
+    
+    def applyChannels(self, user, channel, fromUser, sameUser):
         if user != sameUser:
             return None
         if not channel or fromUser not in channel.users:
             return False
         return None
+    
+    def applyUsers(self, user, fromUser, sameUser):
+        if user != sameUser:
+            return None
+        for channel in fromUser.channels:
+            if user in channel.users:
+                return None
+        return False
 
 invisibleMode = InvisibleMode()
