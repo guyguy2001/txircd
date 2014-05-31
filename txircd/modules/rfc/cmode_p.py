545e19767df2d2a05b55bc18c79e78605197a500
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-05-31 18:08:43 -0500

    Implement channel mode +p

---
+++ txircd/modules/rfc/cmode_p.py
@@ -0,0 +1,30 @@
+from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
+from txircd.module_interface import IMode, IModuleData, Mode, ModuleData
+from txircd.utils import ModeType
+from zope.interface import implements
+
+class PrivateMode(ModuleData, Mode):
+    implements(IPlugin, IModuleData, IMode)
+    
+    name = "PrivateMode"
+    core = True
+    affectedActions = [ "displaychannel" ]
+    
+    def channelModes(self):
+        return [ ("p", ModeType.NoParam, self) ]
+    
+    def actions(self):
+        return [ ("modeactioncheck-channel-p-displaychannel", 1, self.chanIsPrivate) ]
+    
+    def chanIsPrivate(self, channel, displayData, sameChannel, user):
+        if "p" in channel.modes:
+            return True
+        return None
+    
+    def apply(self, actionName, channel, param, displayData, sameChannel, user):
+        if user not in channel.users:
+            displayData["name"] = "*"
+            displayData["modestopic"] = "[]"
+
+privateMode = PrivateMode()
