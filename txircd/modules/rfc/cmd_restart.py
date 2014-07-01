e3f7617d1ec95ef03b0bdb95eea42193c3873b17
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-06-30 22:00:50 -0500

    Implement the RESTART command

---
+++ txircd/modules/rfc/cmd_restart.py
@@ -0,0 +1,38 @@
+from twisted.internet import reactor
+from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
+from txircd.module_interface import Command, ICommand, IModuleData, ModuleData
+from zope.interface import implements
+import os, sys
+
+class RestartCommand(ModuleData, Command):
+    implements(IPlugin, IModuleData, ICommand)
+    
+    name = "RestartCommand"
+    core = True
+    
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+    
+    def actions(self):
+        return [ ("commandpermission-RESTART", 1, self.checkRestartPermission) ]
+    
+    def userCommands(self):
+        return [ ("RESTART", 1, self) ]
+    
+    def checkRestartPermission(self, user, command, data):
+        if not self.ircd.runActionUntilValue("userhasoperpermission", user, "command-restart"):
+            user.sendSingleError("RestartPrivileges", irc.ERR_NOPRIVILEGES, ":Permission denied - You do not have the correct operator privileges")
+            return False
+        return None
+    
+    def parseParams(self, user, params, prefix, tags):
+        return {}
+    
+    def execute(self, user, data):
+        reactor.addSystemEventTrigger("after", "shutdown", lambda: os.execl(sys.executable, sys.executable, *sys.argv))
+        os.unlink("twistd.pid") # If we don't remove the pid file, the new twistd will refuse to start.
+        reactor.stop()
+        return True
+
+restartCmd = RestartCommand()
