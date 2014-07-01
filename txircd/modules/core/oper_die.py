2284789f4f52b6ef190bfdbaebffe54689dc3521
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-06-30 22:14:16 -0500

    Implement the DIE command

---
+++ txircd/modules/core/oper_die.py
@@ -0,0 +1,34 @@
+from twisted.internet import reactor
+from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
+from txircd.module_interface import Command, ICommand, IModuleData, ModuleData
+from zope.interface import implements
+
+class DieCommand(ModuleData, Command):
+    implements(IPlugin, IModuleData, ICommand)
+    
+    name = "DieCommand"
+    core = True
+    
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+    
+    def actions(self):
+        return [ ("commandpermission-DIE", 1, self.checkCommandPermission) ]
+    
+    def userCommands(self):
+        return [ ("DIE", 1, self) ]
+    
+    def checkCommandPermission(self, user, command, data):
+        if not self.ircd.runActionUntilValue("userhasoperpermission", user, "command-die"):
+            return False
+        return None
+    
+    def parseParams(self, user, params, prefix, tags):
+        return {}
+    
+    def execute(self, user, data):
+        reactor.stop()
+        return True
+
+dieCmd = DieCommand()
