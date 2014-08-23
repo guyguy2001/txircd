b9833363ce1067e3c365bbaf208a47f5eb15bd2a
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-08-23 14:08:53 -0500

    Implement the CONNECT command

---
+++ txircd/modules/rfc/cmd_connect.py
@@ -0,0 +1,42 @@
+from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
+from txircd.module_interface import Command, ICommand, IModuleData, ModuleData
+from zope.interface import implements
+
+class ConnectCommand(ModuleData, Command):
+    implements(IPlugin, IModuleData, ICommand)
+    
+    name = "ConnectCommand"
+    core = True
+    
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+    
+    def actions(self):
+        return [ ("commandpermission-CONNECT", 1, self.canConnect) ]
+    
+    def userCommands(self):
+        return [ ("CONNECT", 1, self) ]
+    
+    def canConnect(self, user, command, data):
+        if not self.ircd.runActionUntilValue("userhasoperpermission", "command-connect"):
+            return False
+        return None
+    
+    def parseParams(self, user, params, prefix, tags):
+        if not params:
+            user.sendSingleError("ConnectParams", irc.ERR_NEEDMOREPARAMS, "CONNECT", ":Not enough parameters")
+            return None
+        return {
+            "server": params[0]
+        }
+    
+    def execute(self, user, data):
+        serverName = data["server"]
+        if self.ircd.connectServer(serverName):
+            user.sendMessage("NOTICE", ":*** Connecting to {}".format(serverName))
+        else:
+            user.sendMessage("NOTICE", ":*** Failed to connect to {}".format(serverName))
+        return True
+
+connectCmd = ConnectCommand()
