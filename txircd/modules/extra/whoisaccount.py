9a1d058e6cd31c04b3a4fc665b9a7d0a249813a9
Heufneutje <stefan.frijters@hotmail.com>
2014-09-01 09:54:34 +0000

    Implement the WhoisAccount module

---
+++ txircd/modules/extra/whoisaccount.py
@@ -0,0 +1,20 @@
+from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
+from txircd.module_interface import IModuleData, ModuleData
+from zope.interface import implements
+
+irc.RPL_WHOISACCOUNT = "330"
+
+class WhoisAccount(ModuleData):
+    implements(IPlugin, IModuleData)
+
+    name = "WhoisAccount"
+
+    def actions(self):
+        return [ ("extrawhois", 1, self.whoisAccountName) ]
+
+    def whoisAccountName(self, user, targetUser):
+        if "accountname" in targetUser.metadata["ext"]:
+            user.sendMessage(irc.RPL_WHOISACCOUNT, targetUser.nick, targetUser.metadata["ext"]["accountname"], ":is logged in as")
+
+whoisAccount = WhoisAccount()
