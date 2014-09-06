4b4895eaae4389e3bd0eaf5757dca37dfaf74b04
Heufneutje <stefan.frijters@gmail.com>
2014-09-06 17:39:11 +0200

    Implement the connect and quit server notices

---
+++ txircd/modules/core/sno_connect.py
@@ -0,0 +1,30 @@
+from twisted.plugin import IPlugin
+from txircd.module_interface import IModuleData, ModuleData
+from zope.interface import implements
+
+class SnoConnect(ModuleData):
+    implements(IPlugin, IModuleData)
+
+    name = "ServerNoticeConnect"
+    core = True
+
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+
+    def actions(self):
+        return [ ("register", 1, self.sendConnectNotice),
+                ("servernoticetype", 1, self.checkSnoType)]
+
+    def sendConnectNotice(self, user, *params):
+        message =  "Client connected on {}: {} ({}) [{}]".format(self.ircd.name, user.hostmaskWithRealHost(), user.ip, user.gecos)
+        snodata = {
+            "mask": "connect",
+            "message": message
+        }
+        self.ircd.runActionProcessing("sendservernotice", snodata)
+        return True
+
+    def checkSnoType(self, user, typename):
+        return typename == "connect"
+
+snoConnect = SnoConnect()
