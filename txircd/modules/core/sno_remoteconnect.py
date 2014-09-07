6114e88104d6a4f5bcf43c07828b3f83bb1af0b4
Heufneutje <stefan.frijters@gmail.com>
2014-09-07 10:42:38 +0200

    Add the remoteconnect and remotequit snomasks

---
+++ txircd/modules/core/sno_remoteconnect.py
@@ -0,0 +1,30 @@
+from twisted.plugin import IPlugin
+from txircd.module_interface import IModuleData, ModuleData
+from zope.interface import implements
+
+class SnoRemoteConnect(ModuleData):
+    implements(IPlugin, IModuleData)
+
+    name = "ServerNoticeRemoteConnect"
+    core = True
+
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+
+    def actions(self):
+        return [ ("remoteregister", 1, self.sendRemoteConnectNotice),
+                ("servernoticetype", 1, self.checkSnoType)]
+
+    def sendRemoteConnectNotice(self, user, *params):
+        server = self.ircd.servers[user.uuid[:3]].name
+        message =  "Client connected on {}: {} ({}) [{}]".format(server, user.hostmaskWithRealHost(), user.ip, user.gecos)
+        snodata = {
+            "mask": "connect",
+            "message": message
+        }
+        self.ircd.runActionProcessing("sendservernotice", snodata)
+
+    def checkSnoType(self, user, typename):
+        return typename == "remoteconnect"
+
+snoRemoteConnect = SnoRemoteConnect()
