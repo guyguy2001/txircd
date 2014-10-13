1f8b335e52958e0a2b9ec0fccbc89fafa3d192c3
Heufneutje <stefan.frijters@gmail.com>
2014-10-13 23:19:40 +0200

    Implement the DCC block module

---
+++ txircd/modules/extra/dccblock.py
@@ -0,0 +1,28 @@
+from twisted.plugin import IPlugin
+from txircd.module_interface import IModuleData, ModuleData
+from zope.interface import implements
+
+class DccBlock(ModuleData):
+    implements(IPlugin, IModuleData)
+
+    name = "DccBlock"
+
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+
+    def actions(self):
+        return [ ("commandpermission-PRIVMSG", 1, self.blockDCC),
+                  ("commandpermission-NOTICE", 1, self.blockDCC) ]
+
+    def blockDCC(self, user, command, data):
+        if "targetusers" in data:
+            users = data["targetusers"].keys()
+            dccBlocked = False
+            for targetUser in users:
+                if data["targetusers"][targetUser].upper().startswith("\x01DCC"):
+                    del data["targetusers"][targetUser]
+                    dccBlocked = True
+            if dccBlocked:
+                user.sendMessage("NOTICE", ":DCC is not allowed on this server.")
+
+dccBlock = DccBlock()
