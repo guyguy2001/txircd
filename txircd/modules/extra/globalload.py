8acdafe7c8a2add886b16a681634e352d28e1de0
ElementalAlchemist <ElementAlchemist7@gmail.com>
2014-11-08 19:34:22 -0600

    Add the global load module

---
+++ txircd/modules/extra/globalload.py
@@ -0,0 +1,196 @@
+from twisted.plugin import IPlugin
+from twisted.python import log
+from twisted.words.protocols import irc
+from txircd.module_interface import Command, ICommand, IModuleData, ModuleData
+from zope.interface import implements
+import logging
+
+class GlobalLoad(ModuleData):
+    implements(IPlugin, IModuleData)
+    
+    name = "GlobalLoad"
+    
+    def hookIRCd(self, ircd):
+        self.ircd = ircd
+    
+    def userCommands(self):
+        return [ ("GLOADMODULE", 1, UserLoad(self.ircd)),
+                ("GUNLOADMODULE", 1, UserUnload(self.ircd)),
+                ("GRELOADMODULE", 1, UserReload(self.ircd)) ]
+    
+    def serverCommands(self):
+        return [ ("LOADMODULE", 1, ServerLoad(self.ircd)),
+                ("UNLOADMODULE", 1, ServerUnload(self.ircd)),
+                ("RELOADMODULE", 1, ServerReload(self.ircd)) ]
+
+class UserLoad(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, user, params, prefix, tags):
+        if not params:
+            user.sendMessage(irc.ERR_NEEDMOREPARAMS, "GLOADMODULE", ":Not enough parameters")
+            return None
+        return {
+            "module": params[0]
+        }
+    
+    def execute(self, user, data):
+        moduleName = data["module"]
+        if moduleName in self.ircd.loadedModules:
+            user.sendMessage(irc.ERR_CANTLOADMODULE, moduleName, ":Module is already loaded")
+        else:
+            try:
+                self.ircd.loadModule(moduleName)
+                if moduleName in self.ircd.loadedModules:
+                    user.sendMessage(irc.RPL_LOADEDMODULE, moduleName, ":Module successfully loaded")
+                    for server in self.ircd.servers.itervalues():
+                        if server.nextClosest == self.ircd.serverID:
+                            server.sendMessage("LOADMODULE", moduleName, prefix=user.uuid)
+                else:
+                    user.sendMessage(irc.ERR_CANTLOADMODULE, moduleName, ":No such module")
+            except ModuleLoadError as e:
+                user.sendMessage(irc.ERR_CANTLOADMODULE, moduleName, ":{}".format(e.message))
+        return True
+
+class UserUnload(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, user, params, prefix, tags):
+        if not params:
+            user.sendMessage(irc.ERR_NEEDMOREPARAMS, "GUNLOADMODULE", ":Not enough parameters")
+            return None
+        return {
+            "module": params[0]
+        }
+    
+    def execute(self, user, data):
+        moduleName = data["module"]
+        if moduleName not in self.ircd.loadedModules:
+            user.sendMessage(irc.ERR_CANTUNLOADMODULE, moduleName, ":No such module")
+        else:
+            try:
+                self.ircd.unloadModule(moduleName)
+                user.sendMessage(irc.RPL_UNLOADEDMODULE, moduleName, ":Module successfully unloaded")
+                for server in self.ircd.servers.itervalues():
+                    if server.nextClosest == self.ircd.serverID:
+                        server.sendMessage("UNLOADMODULE", moduleName, prefix=user.uuid)
+            except ValueError as e:
+                user.sendMessage(irc.ERR_CANTUNLOADMODULE, moduleName, ":{}".format(e.message))
+        return True
+
+class UserReload(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, user, params, prefix, tags):
+        if not params:
+            user.sendMessage(irc.ERR_NEEDMOREPARAMS, "GRELOADMODULE", ":Not enough parameters")
+            return None
+        return {
+            "module": params[0]
+        }
+    
+    def execute(self, user, data):
+        moduleName = data["module"]
+        if moduleName not in self.ircd.loadedModules:
+            user.sendMessage(irc.ERR_CANTUNLOADMODULE, moduleName, ":No such module")
+        else:
+            try:
+                self.ircd.reloadModule(moduleName)
+                user.sendMessage(irc.RPL_LOADEDMODULE, moduleName, ":Module successfully reloaded")
+                for server in self.ircd.servers.itervalues():
+                    if server.nextClosest == self.ircd.serverID:
+                        server.sendMessage("RELOADMODULE", moduleName, prefix=user.uuid)
+            except ModuleLoadError as e:
+                user.sendMessage(irc.ERR_CANTUNLOADMODULE, moduleName, ":{}; module is now unloaded".format(e.message))
+        return True
+
+class ServerLoad(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, server, params, prefix, tags):
+        if len(params) != 1:
+            return None
+        return {
+            "from": prefix,
+            "module": params[0]
+        }
+    
+    def execute(self, server, data):
+        fromPrefix = data["from"]
+        moduleName = data["module"]
+        try:
+            self.ircd.loadModule(moduleName)
+        except ModuleLoadError:
+            return None
+        if moduleName not in self.ircd.loadedModules: # We want to log a message, but this shouldn't break the servers
+            log.msg("Tried to globally load nonexistent module {}".format(moduleName), logLevel=logging.WARNING)
+        for remoteServer in self.ircd.servers.itervalues():
+            if remoteServer.nextClosest == self.ircd.serverID and remoteServer != server:
+                remoteServer.sendMessage("LOADMODULE", moduleName, prefix=fromPrefix)
+        return True
+
+class ServerUnload(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, server, params, prefix, tags):
+        if len(params) != 1:
+            return None
+        return {
+            "from": prefix,
+            "module": params[0]
+        }
+    
+    def execute(self, server, data):
+        fromPrefix = data["from"]
+        moduleName = data["module"]
+        try:
+            self.ircd.unloadModule(moduleName)
+        except ValueError:
+            return None
+        for remoteServer in self.ircd.servers.itervalues():
+            if remoteServer.nextClosest == self.ircd.serverID and remoteServer != server:
+                remoteServer.sendMessage("UNLOADMODULE", moduleName, prefix=fromPrefix)
+        return True
+
+class ServerReload(Command):
+    implements(ICommand)
+    
+    def __init__(self, ircd):
+        self.ircd = ircd
+    
+    def parseParams(self, server, params, prefix, tags):
+        if len(params) != 1:
+            return None
+        return {
+            "from": prefix,
+            "module": params[0]
+        }
+    
+    def execute(self, server, data):
+        fromPrefix = data["from"]
+        moduleName = data["module"]
+        try:
+            self.ircd.reloadModule(moduleName)
+        except ModuleLoadError:
+            return None
+        for remoteServer in self.ircd.servers.itervalues():
+            if remoteServer.nextClosest == self.ircd.serverID and remoteServer != server:
+                remoteServer.sendMessage("RELOADMODULE", moduleName, prefix=fromPrefix)
+        return True
+
+globalLoad = GlobalLoad()
