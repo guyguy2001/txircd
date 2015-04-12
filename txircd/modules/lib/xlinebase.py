281b82a26ca537e2f5042adea5d96ef0ae54ff3d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2015-04-11 19:35:07 -0500

    Start an x:line base library class

---
+++ txircd/modules/lib/xlinebase.py
@@ -0,0 +1,60 @@
+from txircd.utils import now, timestamp
+from datetime import timedelta
+
+class XLineBase(object):
+	lineType = None
+	lines = []
+	
+	def matchUser(self, user, context):
+		self.expireLines()
+		for lineData in self.lines:
+			if self.checkUserMatch(user, lineData["mask"]):
+				self.onUserMatch(user, context, lineData["reason"])
+				return True
+		return False
+	
+	def checkUserMatch(self, user, mask):
+		pass
+	
+	def addLine(self, mask, durationSeconds, setter, reason):
+		self.expireLines()
+		if not self.lineType:
+			return
+		for lineData in self.lines:
+			if mask == lineData["mask"]:
+				return 
+		self.lines.append({
+			"mask": mask,
+			"created": now(),
+			"duration": durationSeconds,
+			"setter": setter,
+			"reason": reason
+		})
+	
+	def delLine(self, mask):
+		if not self.lineType:
+			return
+		for index, lineData in enumerate(self.lines):
+			if lineData["mask"] == mask:
+				del self.lines[index]
+				return
+	
+	def onUserMatch(self, user, context, banReason):
+		pass
+	
+	def expireLines(self):
+		currentTime = now()
+		expiredLines = []
+		for lineData in self.lines:
+			duration = timedelta(seconds=lineData["duration"])
+			expireTime = currentTime + duration
+			if expireTime < currentTime:
+				expiredLines.append(lineData)
+		for lineData in expiredLines:
+			self.lines.remove(lineData)
+	
+	def generateInfo(self):
+		lineInfo = {}
+		for lineData in self.lines:
+			lineInfo[lineData["mask"]] = "{} {} {} :{}".format(timestamp(lineData["created"]), lineData["duration"], lineData["setter"], lineData["reason"])
+		return lineInfo
