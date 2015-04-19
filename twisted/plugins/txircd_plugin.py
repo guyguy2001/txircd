963df8697b740037365edb38419c5b85051b726a
ElementalAlchemist <ElementAlchemist7@gmail.com>
2015-04-19 10:43:08 -0500

    I missed some tabs

--- twisted/plugins/txircd_plugin.py
+++ twisted/plugins/txircd_plugin.py
@@ -7,18 +7,18 @@ from zope.interface import implements
 from txircd.ircd import IRCd
 
 class Options(usage.Options):
-    optParameters = [["config", "c", "txircd.yaml", "The configuration file to read"]]
+	optParameters = [["config", "c", "txircd.yaml", "The configuration file to read"]]
 
 class IRCdServiceMaker(object):
-    implements(IServiceMaker, IPlugin)
-    tapname = "txircd"
-    description = "Twisted IRC Server"
-    options = Options
-    
-    def makeService(self, options):
-        ircd = IRCd(options["config"])
-        signal(SIGHUP, lambda signal, stack: ircd.rehash())
-        return ircd
+	implements(IServiceMaker, IPlugin)
+	tapname = "txircd"
+	description = "Twisted IRC Server"
+	options = Options
+	
+	def makeService(self, options):
+		ircd = IRCd(options["config"])
+		signal(SIGHUP, lambda signal, stack: ircd.rehash())
+		return ircd
 
 observer = log.PythonLoggingObserver()
 observer.start()
