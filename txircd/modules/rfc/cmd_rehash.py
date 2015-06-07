0df0aa883a5646b3f7e8c0c7330ccd4d91033acc
ElementalAlchemist <ElementAlchemist7@gmail.com>
2015-06-06 22:56:38 -0500

    Remove the dead PUSH server command from REHASH processing

--- txircd/modules/rfc/cmd_rehash.py
+++ txircd/modules/rfc/cmd_rehash.py
@@ -12,25 +12,20 @@ class RehashCommand(ModuleData):
 	core = True
 	
 	def actions(self):
-		return [ ("commandpermission-REHASH", 1, self.restrictRehashToOpers),
-				("sendremoteusermessage-382", 1, self.pushRehashMessage) ]
+		return [ ("commandpermission-REHASH", 1, self.restrictRehashToOpers) ]
 	
 	def userCommands(self):
 		return [ ("REHASH", 1, UserRehash(self.ircd)) ]
 	
 	def serverCommands(self):
-		return [ ("REHASH", 1, ServerRehash(self.ircd)) ]
+		return [ ("REHASH", 1, ServerRehash(self.ircd)),
+		         ("REHASHNOTICE", 1, ServerRehashNotice(self.ircd)) ]
 	
 	def restrictRehashToOpers(self, user, data):
 		if not self.ircd.runActionUntilValue("userhasoperpermission", user, "command-rehash", users=[user]):
 			user.sendMessage(irc.ERR_NOPRIVILEGES, "Permission denied - You do not have the correct operator privileges")
 			return False
 		return None
-	
-	def pushRehashMessage(self, user, *params, **kw):
-		server = self.ircd.servers[user.uuid[:3]]
-		server.sendMessage("PUSH", user.uuid, ":{} {} {}".format(kw["prefix"], irc.RPL_REHASHING, " ".join(params)))
-		return True
 
 class UserRehash(Command):
 	implements(ICommand)
@@ -99,14 +94,55 @@ class ServerRehash(Command):
 		source = data["source"]
 		if source in self.ircd.users:
 			user = self.ircd.users[source]
-			user.sendMessage(irc.RPL_REHASHING, self.ircd.config.fileName, "Rehashing")
+			self.ircd.servers[user.uuid[:3]].sendMessage("REHASHNOTICE", user.uuid, self.ircd.config.fileName, prefix=self.ircd.serverID)
 		else:
 			user = None
 		try:
 			self.ircd.rehash()
 		except ConfigError as e:
 			if user:
-				user.sendMessage(irc.RPL_REHASHING, self.ircd.config.fileName, "Rehash failed: {}".format(e))
+				self.ircd.servers[user.uuid[:3]].sendMessage("REHASHNOTICE", user.uuid, self.ircd.config.fileName, "Rehash failed: {}".format(e), prefix=self.ircd.serverID)
+		return True
+
+class ServerRehashNotice(Command):
+	implements(ICommand)
+	
+	def __init__(self, ircd):
+		self.ircd = ircd
+	
+	def parseParams(self, server, params, prefix, tags):
+		if len(params) not in (2, 3):
+			return None
+		if params[0] not in self.ircd.users:
+			return None
+		if prefix not in self.ircd.servers:
+			return None
+		if len(params) == 2:
+			return {
+				"fromserver": self.ircd.server[prefix],
+				"user": self.ircd.users[params[0]],
+				"filename": params[1]
+			}
+		return {
+			"fromserver": self.ircd.server[prefix],
+			"user": self.ircd.users[params[0]],
+			"filename": params[1],
+			"message": params[2]
+		}
+	
+	def execute(self, server, data):
+		fromServer = data["fromserver"]
+		toUser = data["user"]
+		if toUser.uuid[:3] == self.ircd.serverID:
+			if "message" in data:
+				toUser.sendMessage(irc.RPL_REHASHING, data["filename"], data["message"], sourceserver=fromServer)
+			else:
+				toUser.sendMessage(irc.RPL_REHASHING, data["filename"], "Rehashing", sourceserver=fromServer)
+			return True
+		if "message" in data:
+			self.ircd.servers[toUser.uuid[:3]].sendMessage("REHASHNOTICE", toUser.uuid, data["filename"], data["message"], prefix=fromServer.serverID)
+		else:
+			self.ircd.servers[toUser.uuid[:3]].sendMessage("REHASHNOTICE", toUser.uuid, data["filename"], prefix=fromServer.serverID)
 		return True
 
 rehashCmd = RehashCommand()
