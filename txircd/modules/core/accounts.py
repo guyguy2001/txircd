904db705daf24d68fcc9ac6010b55b93c7dc4544
ElementalAlchemist <ElementAlchemist7@gmail.com>
2015-06-28 22:39:56 -0500

    Add automatic sending of 900/901 numerics for account status

--- txircd/modules/core/accounts.py
+++ txircd/modules/core/accounts.py
@@ -1,7 +1,12 @@
 from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
 from txircd.module_interface import IModuleData, ModuleData
 from txircd.utils import ircLower
 from zope.interface import implements
+
+# Numerics and names are taken from the IRCv3.1 SASL specification at http://ircv3.net/specs/extensions/sasl-3.1.html
+irc.RPL_LOGGEDIN = "900"
+irc.RPL_LOGGEDOUT = "901"
 
 class Accounts(ModuleData):
 	implements(IPlugin, IModuleData)
@@ -10,11 +15,19 @@ class Accounts(ModuleData):
 	core = True
 	
 	def actions(self):
-		return [ ("usercansetmetadata", 10, self.denyMetadataSet) ]
+		return [ ("usercansetmetadata", 10, self.denyMetadataSet),
+		         ("usermetadataupdate", 10, self.sendLoginNumeric) ]
 	
 	def denyMetadataSet(self, key):
 		if ircLower(key) == "account":
 			return False
 		return None
+	
+	def sendLoginNumeric(self, user, key, oldValue, value, visibility, setByUser, fromServer):
+		if key == "account":
+			if value is None:
+				user.sendMessage(irc.RPL_LOGGEDOUT, user.hostmask(), "You are now logged out")
+			else:
+				user.sendMessage(irc.RPL_LOGGEDIN, user.hostmask(), value, "You are now logged in as {}".format(value))
 
 accounts = Accounts()
