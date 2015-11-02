8256b9613155c85bde92e4e6b2c0e452c8bc9da6
ElementalAlchemist <ElementAlchemist7@gmail.com>
2015-11-01 20:42:29 -0600

    Make the ratelimit module's configuration consistent with the rest of it

--- txircd/modules/extra/ratelimit.py
+++ txircd/modules/extra/ratelimit.py
@@ -13,28 +13,34 @@ class RateLimit(ModuleData):
 		return [ ("commandpermission", 100, self.recvCommand) ]
 
 	def verifyConfig(self, config):
-		if "ratelimit" in config:
-			if not isinstance(config["ratelimit"], dict):
-				raise ConfigValidationError("ratelimit", "value must be a dictionary")
-			for key, value in config["ratelimit"].itemitems():
-				if key not in ("limit", "kill_limit", "interval"):
-					continue
-				if not isinstance(value, int) or value < 0: # We might want to do some checking for insane values here
-					raise ConfigValidationError("ratelimit", "value \"{}\" is an invalid number".format(key))
-
-	def getConfig(self):
-		config = {
-			"limit": 60, # stop accepting commands after this many
-			"kill_limit": 500, # disconnect the user after this many
-			"interval": 60,
-		}
-		config.update(self.ircd.config.get("ratelimit", {}))
-		return config
+		if "rate_soft_limit" in config:
+			if not isinstance(config["rate_soft_limit"], int) or config["rate_soft_limit"] < 0:
+				raise ConfigValidationError("rate_soft_limit", "invalid number")
+			if config["rate_soft_limit"] == 0:
+				self.ircd.logConfigValidationWarning("rate_soft_limit", "a value of 0 will block everything", 1)
+				config["rate_soft_limit"] = 1
+		else:
+			config["rate_soft_limit"] = 60
+		
+		if "rate_kill_limit" in config:
+			if not isinstance(config["rate_kill_limit"], int) or config["rate_kill_limit"] < 0:
+				raise ConfigValidationError("rate_kill_limit", "invalid number")
+			if config["rate_kill_limit"] == 0:
+				self.ircd.logConfigValidationWarning("rate_kill_limit", "a value of 0 will kill everyone; what's the point of having a server?", 1)
+				config["rate_kill_limit"] = 1
+		else:
+			config["rate_kill_limit"] = 500
+		
+		if "rate_interval" in config:
+			if not isinstance(config["rate_interval"], int) or config["rate_interval"] < 0:
+				raise ConfigValidationError("rate_interval", "invalid number")
+		else:
+			config["rate_interval"] = 60
 
 	def getPeriodData(self):
 		"""Returns (period as integer, time to end of period)"""
 		nowTS = timestamp(now())
-		interval = self.getConfig()["interval"]
+		interval = self.ircd.config["rate_interval"]
 		period = int(nowTS / interval)
 		timeToEnd = (period + 1) * interval - nowTS
 		return period, timeToEnd
@@ -47,16 +53,18 @@ class RateLimit(ModuleData):
 			rateData["messages"] = 0
 			rateData["period"] = thisPeriod
 			rateData["noticeSent"] = False
+		killLimit = self.ircd.config["rate_kill_limit"]
 		rateData["messages"] += 1
-		if rateData["messages"] > self.getConfig()["kill_limit"]:
+		if rateData["messages"] > killLimit:
 			user.disconnect("Killed: Flooding")
 			return False
-		if rateData["messages"] > self.getConfig()["limit"]:
+		softLimit = self.ircd.config["rate_soft_limit"]
+		if rateData["messages"] > softLimit:
 			# only send notice once per period
 			if not rateData["noticeSent"]:
 				user.sendMessage("NOTICE", ("You are sending too many messages (limit is {limit}/{interval:.2f}s). "
 				                            "You cannot send any more messages for {timeToEnd:.2f} seconds."
-				                           ).format(timeToEnd=timeToEnd, **self.getConfig()))
+				                           ).format(limit=softLimit, interval=self.ircd.config["rate_interval"], timeToEnd=timeToEnd))
 				self.ircd.log.info("User {user.uuid} ({user.nick}) exceeded the message limit", user=user)
 				rateData["noticeSent"] = True
 			# we whitelist ping/pong to prevent ping timeouts
