df94e4977155292b2ba2304d4d44a580d2817a5d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2016-01-15 22:03:00 -0600

    Update +p and +s channel modes to affect WHOIS output

--- txircd/modules/rfc/cmode_p.py
+++ txircd/modules/rfc/cmode_p.py
@@ -8,24 +8,39 @@ class PrivateMode(ModuleData, Mode):
 	
 	name = "PrivateMode"
 	core = True
-	affectedActions = { "displaychannel": 10 }
+	affectedActions = { "displaychannel": 10,
+	                    "showchannel-whois": 10 }
 	
 	def channelModes(self):
 		return [ ("p", ModeType.NoParam, self) ]
 	
 	def actions(self):
-		return [ ("modeactioncheck-channel-p-displaychannel", 1, self.chanIsPrivate) ]
+		return [ ("modeactioncheck-channel-p-displaychannel", 1, self.chanIsPrivateList),
+		         ("modeactioncheck-channel-p-showchannel-whois", 1, self.chanIsPrivateWhois) ]
 	
-	def chanIsPrivate(self, channel, displayData, sameChannel, user, usedSearchMask):
+	def chanIsPrivateList(self, channel, displayData, sameChannel, user, usedSearchMask):
 		if "p" in channel.modes:
 			return True
 		return None
 	
-	def apply(self, actionName, channel, param, displayData, sameChannel, user, usedSearchMask):
-		if usedSearchMask:
-			displayData.clear()
-		elif user not in channel.users:
-			displayData["name"] = "*"
-			displayData["modestopic"] = "[]"
+	def chanIsPrivateWhois(self, channel, sameChannel, queryUser, targetUser):
+		if "p" in channel.modes:
+			return True
+		return None
+	
+	def apply(self, actionName, channel, param, *params):
+		if actionName == "displaychannel":
+			displayData, sameChannel, user, usedSearchMask = params
+			if usedSearchMask:
+				displayData.clear()
+			elif user not in channel.users:
+				displayData["name"] = "*"
+				displayData["modestopic"] = "[]"
+			return
+		if actionName == "showchannel-whois":
+			sameChannel, queryUser, targetUser = params
+			if queryUser not in channel.users:
+				return False
+			return None
 
 privateMode = PrivateMode()
--- txircd/modules/rfc/cmode_s.py
+++ txircd/modules/rfc/cmode_s.py
@@ -8,21 +8,36 @@ class SecretMode(ModuleData, Mode):
 	
 	name = "SecretMode"
 	core = True
-	affectedActions = { "displaychannel": 20 }
+	affectedActions = { "displaychannel": 20,
+	                    "showchannel-whois": 20 }
 	
 	def channelModes(self):
 		return [ ("s", ModeType.NoParam, self) ]
 	
 	def actions(self):
-		return [ ("modeactioncheck-channel-s-displaychannel", 1, self.chanIsSecret) ]
+		return [ ("modeactioncheck-channel-s-displaychannel", 1, self.chanIsSecretList),
+		         ("modeactioncheck-channel-s-showchannel-whois", 1, self.chanIsSecretWhois) ]
 	
-	def chanIsSecret(self, channel, displayData, sameChannel, user, usedSearchMask):
+	def chanIsSecretList(self, channel, displayData, sameChannel, user, usedSearchMask):
 		if "s" in channel.modes:
 			return True
 		return None
 	
-	def apply(self, actionName, channel, param, displayData, sameChannel, user, usedSearchMask):
-		if user not in channel.users:
-			displayData.clear() # Let's make it not show the channel at all
+	def chanIsSecretWhois(self, channel, sameChannel, queryUser, targetUser):
+		if "s" in channel.modes:
+			return True
+		return None
+	
+	def apply(self, actionName, channel, param, *params):
+		if actionName == "displaychannel":
+			displayData, sameChannel, user, usedSearchMask = params
+			if user not in channel.users:
+				displayData.clear() # Let's make it not show the channel at all
+			return
+		if actionName == "showchannel-whois":
+			sameChannel, queryUser, targetUser = params
+			if queryUser not in channel.users:
+				return False
+			return None
 
 secretMode = SecretMode()
