472f03e9d611c5177a91a2cd477dda7246665c85
ElementalAlchemist <ElementAlchemist7@gmail.com>
2016-01-23 11:21:02 -0600

    Have Manhole rebind on rehash

--- txircd/modules/debug/manhole.py
+++ txircd/modules/debug/manhole.py
@@ -9,8 +9,28 @@ class Manhole(ModuleData):
 	name = "Manhole"
 	
 	manhole = None
+	deferredStop = None
 	
 	def load(self):
+		self.startManhole()
+	
+	def unload(self):
+		if self.deferredStop and not self.deferredStop.called:
+			self.deferredStop.addCallback(lambda result: self.manhole.stopService())
+			return self.deferredStop
+		return self.manhole.stopService()
+	
+	def rehash(self):
+		if self.deferredStop and not self.deferredStop.called:
+			return # The deferred's callbacks should handle the rest of the rehash
+		d = self.manhole.stopService()
+		if d:
+			d.addCallback(self.startManhole)
+			self.deferredStop = d
+		else:
+			self.startManhole()
+	
+	def startManhole(self, result = None):
 		self.manhole = makeService({
 			"namespace": { "ircd": self.ircd },
 			"passwd": self.ircd.config.get("manhole_passwd", "manhole.passwd"),
@@ -18,8 +38,5 @@ class Manhole(ModuleData):
 			"sshPort": self.ircd.config.get("manhole_bind_ssh", None)
 		})
 		self.manhole.startService()
-	
-	def unload(self):
-		return self.manhole.stopService()
 
 manhole = Manhole()
