320196701c2c36138c254329e3f5ae1f0c63bbe7
ElementalAlchemist <ElementAlchemist7@gmail.com>
2016-06-19 16:39:17 -0500

    Suppress remoteconnect messages for netjoin

--- txircd/modules/extra/snotice_remoteconnect.py
+++ txircd/modules/extra/snotice_remoteconnect.py
@@ -4,23 +4,36 @@ from zope.interface import implements
 
 class SnoRemoteConnect(ModuleData):
 	implements(IPlugin, IModuleData)
-
+	
 	name = "ServerNoticeRemoteConnect"
-
+	
+	def __init__(self):
+		self.burstingServer = None
+	
 	def actions(self):
 		return [ ("remoteregister", 1, self.sendRemoteConnectNotice),
-		         ("servernoticetype", 1, self.checkSnoType)]
-
+		         ("servernoticetype", 1, self.checkSnoType),
+		         ("startburstcommand", 1, self.markStartBurst),
+		         ("endburstcommand", 1, self.markEndBurst) ]
+	
 	def sendRemoteConnectNotice(self, user, *params):
 		server = self.ircd.servers[user.uuid[:3]].name
+		if server == self.burstingServer:
+			return
 		message =  "Client connected on {}: {} ({}) [{}]".format(server, user.hostmaskWithRealHost(), user.ip, user.gecos)
 		snodata = {
 			"mask": "connect",
 			"message": message
 		}
 		self.ircd.runActionProcessing("sendservernotice", snodata)
-
+	
 	def checkSnoType(self, user, typename):
 		return typename == "remoteconnect"
+	
+	def markStartBurst(self, server, command):
+		self.burstingServer = server
+	
+	def markEndBurst(self, server, command):
+		self.burstingServer = None
 
 snoRemoteConnect = SnoRemoteConnect()
