e4518cee0ebdb4c1c4fca964c130b2520020ba17
ElementalAlchemist <ElementAlchemist7@gmail.com>
2016-11-17 20:17:50 -0600

    Implement register command

---
+++ txircd/modules/extra/services/account_register.py
@@ -0,0 +1,47 @@
+from twisted.plugin import IPlugin
+from twisted.words.protocols import irc
+from txircd.module_interface import Command, ICommand, IModuleData, ModuleData
+from zope.interface import implements
+from validate_email import validate_email as validateEmail
+
+irc.ERR_SERVICES = "955" # Custom numeric; 955 <TYPE> <SUBTYPE> <ERROR>
+
+class AccountRegister(ModuleData, Command):
+	implements(IPlugin, IModuleData, ICommand)
+	
+	name = "AccountRegister"
+	
+	def userCommands(self):
+		return [ ("REGISTER", 1, self) ]
+	
+	def parseParams(self, user, params, prefix, tags):
+		if not params:
+			user.sendSingleError("RegisterParams", irc.ERR_NEEDMOREPARAMS, "REGISTER", "Not enough parameters")
+			return None
+		if len(params) >= 2:
+			emailAddr = params[1]
+			if not validateEmail(emailAddr):
+				user.startErrorBatch("RegisterEmail")
+				user.sendBatchedError("RegisterEmail", irc.ERR_SERVICES, "ACCOUNT", "EMAIL", "The entered email address is invalid.")
+				user.sendBatchedError("RegisterEmail", "NOTICE", "The entered email address is invalid.")
+				return None
+			return {
+				"password": params[0],
+				"email": params[1]
+			}
+		if self.ircd.config.get("account_email_required", False):
+			user.sendSingleError("RegisterEmail", irc.ERR_NEEDMOREPARAMS, "REGISTER", "Not enough parameters (email address required)")
+			return None
+		return {
+			"password": params[0]
+		}
+	
+	def execute(self, user, data):
+		createResult = self.ircd.runActionUntilValue("createnewaccount", user.nick, data["password"], None, data["email"] if "email" in data else None)
+		if createResult[0]:
+			return True
+		user.sendMessage(irc.ERR_SERVICES, "ACCOUNT", "CREATE", createResult[1])
+		user.sendMessage("NOTICE", "Your account couldn't be registered: {}".format(createResult[1]))
+		return True
+
+registerCommand = AccountRegister()
