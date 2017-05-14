eed3780606a0224b46c0619a7d70ac307adf0991
ElementalAlchemist <ElementAlchemist7@gmail.com>
2017-05-14 10:36:55 -0500

    Remove the deprecated METADATA command and metadata-notify capability

--- txircd/modules/ircv3/monitor.py
+++ txircd/modules/ircv3/monitor.py
@@ -20,16 +20,12 @@ class Monitor(ModuleData, Command):
 	name = "Monitor"
 	
 	def actions(self):
-		return [ ("capabilitylist", 10, self.addCapability),
-		         ("welcome", 1, self.reportNewUser),
+		return [ ("welcome", 1, self.reportNewUser),
 		         ("remoteregister", 1, self.reportNewUser),
 		         ("changenick", 1, self.reportNickChangeUser),
 		         ("remotechangenick", 1, self.reportNickChangeUser),
 		         ("quit", 1, self.reportGoneUser),
 		         ("remotequit", 1, self.reportGoneUser),
-		         ("addusercap", 1, self.userSubMetadata),
-		         ("usermetadataupdate", 1, self.notifyUserMetadataChange),
-		         ("channelmetadataupdate", 1, self.notifyChannelMetadataChange),
 		         ("buildisupport", 1, self.buildISupport) ]
 	
 	def userCommands(self):
@@ -44,21 +40,10 @@ class Monitor(ModuleData, Command):
 		# memory getting full.
 		self.indexCleaner = LoopingCall(self.cleanIndex)
 		self.indexCleaner.start(60, now=False)
-		if "unloading-monitor" in self.ircd.dataCache:
-			del self.ircd.dataCache["unloading-monitor"]
-			return
-		if "cap-add" in self.ircd.functionCache:
-			self.ircd.functionCache["cap-add"]("metadata-notify")
 	
 	def unload(self):
 		if self.indexCleaner.running:
 			self.indexCleaner.stop()
-		self.ircd.dataCache["unloading-monitor"] = True
-	
-	def fullUnload(self):
-		del self.ircd.dataCache["unloading-monitor"]
-		if "cap-del" in self.ircd.functionCache:
-			self.ircd.functionCache["cap-del"]("metadata-notify")
 	
 	def verifyConfig(self, config):
 		if "monitor_limit" not in config:
@@ -66,9 +51,6 @@ class Monitor(ModuleData, Command):
 			return
 		if not isinstance(config["monitor_limit"], int) or config["monitor_limit"] < 0:
 			raise ConfigValidationError("monitor_limit", "invalid number")
-	
-	def addCapability(self, user, capList):
-		capList.append("metadata-notify")
 	
 	def reportNewUser(self, user):
 		self._doNotify(user.nick, irc.RPL_MONONLINE)
@@ -86,58 +68,6 @@ class Monitor(ModuleData, Command):
 			for notifyUser in self.ircd.dataCache["monitor-index"][nick]:
 				notifyUser.sendMessage(numeric, nick)
 	
-	def userSubMetadata(self, user, capability, value):
-		if capability != "metadata-notify":
-			return
-		self.sendUserMetadata(user, user)
-		if "monitor" in user.cache:
-			for target in user.cache["monitor"]:
-				if target in self.ircd.userNicks:
-					targetUser = self.ircd.users[self.ircd.userNicks[target]]
-					self.sendUserMetadata(targetUser, user)
-	
-	def notifyUserMetadataChange(self, user, key, oldValue, value, visibility, setByUser, fromServer):
-		if not user.isRegistered():
-			return
-		sentToUsers = set()
-		if not setByUser and ("capabilities" in user.cache and "metadata-notify" in user.cache["capabilities"]) and user.canSeeMetadataVisibility(visibility):
-			# Technically, the spec excludes "changes made by the clients themselves" from notification. However,
-			# since we don't know WHICH user changed the metadata, we'll exclude all sets by users and hope that
-			# nobody's actually changing someone else's metadata (would only be opers).
-			if value is None:
-				user.sendMessage("METADATA", key, visibility, to=user.nick)
-			else:
-				user.sendMessage("METADATA", key, visibility, value, to=user.nick)
-			sentToUsers.add(user)
-		if user.nick in self.ircd.dataCache["monitor-index"]:
-			for monitoringUser in self.ircd.dataCache["monitor-index"][user.nick]:
-				if monitoringUser in sentToUsers:
-					continue
-				if "capabilities" in monitoringUser.cache and "metadata-notify" in monitoringUser.cache["capabilities"] and monitoringUser.canSeeMetadataVisibility(visibility):
-					if value is None:
-						monitoringUser.sendMessage("METADATA", key, visibility, to=user.nick)
-					else:
-						monitoringUser.sendMessage("METADATA", key, visibility, value, to=user.nick)
-					sentToUsers.add(monitoringUser)
-		for channel in user.channels:
-			for inChannelUser in channel.users.iterkeys():
-				if inChannelUser in sentToUsers:
-					continue
-				if "capabilities" in inChannelUser.cache and "metadata-notify" in inChannelUser.cache["capabilities"] and inChannelUser.canSeeMetadataVisibility(visibility):
-					if value is None:
-						inChannelUser.sendMessage("METADATA", key, visibility, to=user.nick)
-					else:
-						inChannelUser.sendMessage("METADATA", key, visibility, value, to=user.nick)
-					sentToUsers.add(inChannelUser)
-	
-	def notifyChannelMetadataChange(self, channel, key, oldValue, value, visibility, setByUser, fromServer):
-		for user in channel.users.iterkeys():
-			if "capabilities" in user.cache and "metadata-notify" in user.cache["capabilities"] and user.canSeeMetadataVisibility(visibility):
-				if value is None:
-					user.sendMessage("METADATA", key, visibility, to=channel.name)
-				else:
-					user.sendMessage("METADATA", key, visibility, value, to=channel.name)
-	
 	def buildISupport(self, data):
 		data["MONITOR"] = self.ircd.config["monitor_limit"]
 	
@@ -148,13 +78,6 @@ class Monitor(ModuleData, Command):
 				removeKeys.append(target)
 		for target in removeKeys:
 			del self.ircd.dataCache["monitor-index"][target]
-	
-	def sendUserMetadata(self, user, sendToUser):
-		metadataList = user.metadataList()
-		for key, value, visibility, setByUser in metadataList:
-			if sendToUser.canSeeMetadataVisibility(visibility):
-				sendToUser.sendMessage(irc.RPL_KEYVALUE, user.nick, key, visibility, value)
-		sendToUser.sendMessage(irc.RPL_METADATAEND, "end of metadata")
 	
 	def parseParams(self, user, params, prefix, tags):
 		if not params:
