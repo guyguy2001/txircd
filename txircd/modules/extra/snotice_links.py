bf2d457ae8173a1372bb0175bc8a08063c8e8e85
ElementalAlchemist <ElementAlchemist7@gmail.com>
2020-01-09 09:15:38 -0600

    Improve usefulness of server links disconnect messages if a server didn't start registering yet

--- txircd/modules/extra/snotice_links.py
+++ txircd/modules/extra/snotice_links.py
@@ -17,7 +17,10 @@ class SnoLinks(ModuleData):
 		self.ircd.runActionStandard("sendservernotice", "links", message)
 	
 	def announceQuit(self, server: "IRCServer", reason: str) -> None:
-		message = "Server {} ({}) disconnected (from {}) ({})".format(server.name, server.serverID, self.ircd.name if server.nextClosest == self.ircd.serverID else self.ircd.servers[server.nextClosest].name, reason)
+		if server.serverID:
+			message = "Server {} ({}) disconnected (from {}) ({})".format(server.name, server.serverID, self.ircd.name if server.nextClosest == self.ircd.serverID else self.ircd.servers[server.nextClosest].name, reason)
+		else:
+			message = "Unregistered server at {} disconnected (from {}) ({})".format(server.ip, self.ircd.name if server.nextClosest == self.ircd.serverID else self.ircd.servers[server.nextClosest].name, reason)
 		self.ircd.runActionStandard("sendservernotice", "links", message)
 	
 	def checkSnoType(self, user: "IRCUser", typename: str) -> bool:
